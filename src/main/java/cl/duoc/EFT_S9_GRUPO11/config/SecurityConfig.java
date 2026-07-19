6764d79a4efcee181499eaaa49bac27b08375789
Maria Gracia Brambilla <ma.brambilla@duocuc.cl>
2026-07-19 12:51:54 -0400

    Refactor SecurityConfig to simplify authorization
    Removed JWT authentication configuration and adjusted request authorization.

--- src/main/java/cl/duoc/EFT_S9_GRUPO11/config/SecurityConfig.java
+++ src/main/java/cl/duoc/EFT_S9_GRUPO11/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,28 +15,9 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                
-                .requestMatchers("/api/guias/**").hasAuthority("ROLE_ESTUDIANTE")
-                .anyRequest().authenticated()
-            )
-            // Aquí activamos la validación JWT
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                .anyRequest().permitAll()
             );
-            
+
         return http.build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-        // Le decimos a Spring que busque el rol en "jobTitle"
-        converter.setAuthoritiesClaimName("jobTitle");
-        // no necesitamos prefijo extra
-        converter.setAuthorityPrefix(""); 
-
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
-        return jwtConverter;
     }
 }
